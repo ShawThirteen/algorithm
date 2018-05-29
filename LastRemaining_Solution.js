1bf8c58d187875c12f3d533998017ac538c3fd45
ShawThirteen <905519160@qq.com>
2018-05-29 10:09:44 +0800

    简化代码

--- LastRemaining_Solution.js
+++ LastRemaining_Solution.js
@@ -15,12 +15,8 @@ function LastRemaining_Solution(n, m)
     while (arr.length > 0) {     // 创建一个循环
         if (arr.length == 1) {     // 如果数组只剩下一个,则返回
             return arr[0];
-        } else {                              //如果数组长度大一1
-            // 此处的if放在index = index - 1 + m之下也可以
-            // 当首次m > n时,移除一个空,然后会经过index >= arr.length的判断,又会走的正常的计算,只是多走了一步无效步骤
-            if (index >= arr.length) {        // 首先判断当前剪切点索引是否大于数组长度,比如4个人,第一个报数到5的人出局,
-                index = index % arr.length;   // 如果大于,则取余
-            }
+        } else {                              //如果数组长度大余1
+            index = index % arr.length;   // 判断index是否大于长度,如果大于则取余
             arr.splice(index, 1);         // 移除一人
             index = index - 1 + m;        // 因为删了一个数,数组长度少1,所以index-1使位置回到删除前的序列,再加上m
         }
